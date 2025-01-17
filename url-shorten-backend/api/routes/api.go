072b9ab4ccb7b9fb9d02709122a24a90c777c73d
DamikaAlwis <damikaa.20@cse.mrt.ac.lk>
2025-01-17 13:06:22 +0530

    update setup routes to use ratelimit service and url service

--- url-shorten-backend/api/routes/api.go
+++ url-shorten-backend/api/routes/api.go
@@ -7,13 +7,13 @@ import (
 
 
 
-func SetupRoutes(r *gin.Engine, srv *service.Service){
+func SetupRoutes(r *gin.Engine, srv *service.AppService){
 	// define routes
-	r.POST("/shorten",middleware.RateLimitMiddleware(srv), func( c *gin.Context){
-		shortenURL(c, srv)
+	r.POST("/shorten",middleware.RateLimitMiddleware(srv.RateLimitService), func( c *gin.Context){
+		shortenURL(c, srv.URLService, srv.RateLimitService)
 	})
 	r.GET("/:short_code", func(c *gin.Context){
-		resolveURL(c, srv)
+		resolveURL(c, srv.URLService)
 	})
 	r.GET("/", func(c *gin.Context){
 		c.JSON(200, gin.H{"message": "Welcome to the URL shortening service!"})
