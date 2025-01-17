10da9aac88c444e175cdccf281e6750b13372361
DamikaAlwis <damikaa.20@cse.mrt.ac.lk>
2025-01-17 12:44:27 +0530

    use URL service

--- url-shorten-backend/api/routes/resolve.go
+++ url-shorten-backend/api/routes/resolve.go
@@ -1,46 +1,53 @@
 package routes
 
 import (
-	"context"
+	// "context"
 	"errors"
-	"fmt"
+	// "fmt"
 	"net/http"
-	"time"
+	// "time"
 	"github.com/DamikaAlwis-Gif/shorten-url-app/custom_errors"
-	"github.com/DamikaAlwis-Gif/shorten-url-app/repository"
+	// "github.com/DamikaAlwis-Gif/shorten-url-app/repository"
 	"github.com/DamikaAlwis-Gif/shorten-url-app/service"
 	"github.com/gin-gonic/gin"
 )
 
-func resolveURL(c *gin.Context, srv *service.Service){
+func resolveURL(c *gin.Context, srv *service.URLService){
 	ctx := c.Request.Context()
-	shortURL := c.Param("short_code")
+	shortCode := c.Param("short_code")
 
-
-	
 	// Resolve the URL
-  originalURL, err := repository.ResolveShortURL(ctx, srv, shortURL)
+	originalURL , err := srv.ResolveShortURL(ctx, shortCode)
 	
   if err != nil {
     if errors.Is(err, custom_errors.ErrShortURLNotFound) {
-        c.JSON(http.StatusNotFound, gin.H{"error": "Short URL not found"})
-    } else {
+      c.JSON(http.StatusNotFound, gin.H{"error": "Short URL not found"})
+			return
+				
+    }else if errors.Is(err, custom_errors.ErrURLExpired){
+			c.JSON(http.StatusGone, gin.H{"error": "URL has expired"})
+			return
+		}else {
       c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+			return
     }
-    return
-  }
 
-	go func(){
+  }else{
+		c.Redirect(http.StatusMovedPermanently, originalURL)
+	}
+	
 
-		logCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-    defer cancel()
-		err := repository.LogClick(logCtx,srv, shortURL, c.ClientIP(), c.Request.UserAgent())
-		if err!= nil {
-			fmt.Printf("Error logging click for short URL %s: %v\n", shortURL, err)
-		}
-	}()
-
-	// redirect to the original url
-	c.Redirect(http.StatusMovedPermanently, originalURL)
 	
 }
+
+// go func(){
+
+	// 	logCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+  //   defer cancel()
+	// 	err := repository.LogClick(logCtx,srv, shortURL, c.ClientIP(), c.Request.UserAgent())
+	// 	if err!= nil {
+	// 		fmt.Printf("Error logging click for short URL %s: %v\n", shortURL, err)
+	// 	}
+	// }()
+
+	// redirect to the original url
