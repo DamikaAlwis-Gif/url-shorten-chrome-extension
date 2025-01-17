f6e4422e22d0b083b006795333ddaefd46d72e9d
DamikaAlwis <damikaa.20@cse.mrt.ac.lk>
2025-01-17 12:11:19 +0530

    add new custom errors : ErrURLExpired, ErrKeyNotFound, ErrRateLimitExceeded

--- url-shorten-backend/api/custom_errors/custom_errors.go
+++ url-shorten-backend/api/custom_errors/custom_errors.go
@@ -1,6 +1,22 @@
 package custom_errors
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"time"
+)
 
 var ErrShortKeyExists = errors.New("short key already exists")
 var ErrShortURLNotFound = errors.New("short url not found")
+var ErrURLExpired = errors.New("url expired")
+var ErrKeyNotFound = errors.New("key not found")
+// var ErrRateLimitExceeded = errors.New("rate limit exceeded")
+
+type ErrRateLimitExceeded struct {
+	TTL time.Duration // Time-to-live in seconds
+}
+
+func (e *ErrRateLimitExceeded) Error() string {
+	return fmt.Sprintf("Rate limit exceeded, try again in %v minutes", e.TTL.Minutes())
+}
+
